35c11be0b123a08a488a694a023828ca37795d39
haruki7049 <tontonkirikiri@gmail.com>
2024-10-18 13:47:24 +0900

    Initialize flake-compat

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-compat.url = "github:edolstra/flake-compat";
   };
 
   outputs =
